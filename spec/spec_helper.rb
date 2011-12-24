f8d08e7a8c243517d8df9c822f02988c2a048a99
Xavier Shay <xavier@squareup.com>
2011-12-23 19:31:56 -0800

    Add code coverage to build.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,17 @@ def make_file(content)
   tempfile.flush
   tempfile.path
 end
+
+require 'simplecov'
+
+class SimpleCov::Formatter::QualityFormatter
+  def format(result)
+    SimpleCov::Formatter::HTMLFormatter.new.format(result)
+    File.open("coverage/covered_percent", "w") do |f|
+      f.puts result.source_files.covered_percent.to_i
+    end
+  end
+end
+
+SimpleCov.formatter = SimpleCov::Formatter::QualityFormatter
+SimpleCov.start
