add4f460ccaf1f85381d3d2e554cdcdf73c5b101
Xavier Shay <xavier@squareup.com>
2011-12-23 18:22:44 -0800

    Move finding problems into FileLine.

--- lib/cane/style_check.rb
+++ lib/cane/style_check.rb
@@ -9,11 +9,22 @@ module Tailor
   class FileLine
     def print_problem(message)
       message = message.gsub(/\[.+\]\s+/, '')
-      problems << StyleViolation.new(@file_path, @line_number, message)
+      @problems << StyleViolation.new(@file_path, @line_number, message)
     end
 
     def problems
-      @problems ||= []
+      @problems = []
+      find_problems
+      @problems
+    end
+
+    def find_problems
+      # This is weird. These methods actually have side-effects! We capture
+      # the effects my monkey-patching FileLine above.
+      spacing_problems
+      method_line? && camel_case_method?
+      class_line?  && snake_case_class?
+      too_long?
     end
   end
 end
@@ -31,16 +42,7 @@ module Cane
       file_path = Pathname.new(file_name)
 
       source.each_line.map.with_index do |source_line, line_number|
-        line = Tailor::FileLine.new(source_line, file_path, line_number + 1)
-
-        # This is weird. These methods actually have side-effects! We capture
-        # the effects my monkey-patching FileLine above.
-        line.spacing_problems
-        line.method_line? && line.camel_case_method?
-        line.class_line? && line.snake_case_class?
-        line.too_long?
-
-        line.problems
+        Tailor::FileLine.new(source_line, file_path, line_number + 1).problems
       end
     end
   end
