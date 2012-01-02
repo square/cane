e684e7135ffe8db36efb7f958f02488a46d73d35
Xavier Shay <xavier@squareup.com>
2012-01-02 11:58:24 -0800

    Prevent tempfiles from being GC'ed while they are still in use.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,12 @@
 require 'tempfile'
 
+# Keep a reference to all tempfiles so they are not garbage collected until the
+# process exits.
+$tempfiles = []
+
 def make_file(content)
   tempfile = Tempfile.new('cane')
+  $tempfiles << tempfile
   tempfile.print(content)
   tempfile.flush
   tempfile.path
