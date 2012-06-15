ced755f93921cf95c2023100293b1be26efbe425
James Healy <jimmy@deefa.com>
2012-06-15 22:06:03 +1000

    include total violation count in ViolationFormatter output
    * When adding cane to a legacy project and ratching down the
      max_violations setting it's handy to know what value to set
    * At the moment I need to manually sum the violation count for each
      grouping

--- spec/violation_formatter_spec.rb
+++ spec/violation_formatter_spec.rb
@@ -13,4 +13,10 @@ describe Cane::ViolationFormatter do
   it 'includes number of violations in the group header' do
     described_class.new([violation("FAIL")]).to_s.should include("(1)")
   end
+
+  it 'includes total number of violations' do
+    violations = [violation("FAIL1"),violation("FAIL2")]
+    result = described_class.new(violations).to_s
+    result.should include("Total Violations: 2")
+  end
 end
