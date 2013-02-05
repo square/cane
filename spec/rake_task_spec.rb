4178d5c525aba9e70edc11b6245a6c20de3d5107
Xavier Shay <xavier@rhnh.net>
2013-02-05 07:33:30 -0800

    Remove stubbing from integration test.

--- spec/rake_task_spec.rb
+++ spec/rake_task_spec.rb
@@ -48,19 +48,19 @@ describe Cane::RakeTask do
   end
 
   it 'defaults to using a canefile without a block' do
-    conf = "--gte 90,99"
-    conf_file = make_file conf
+    in_tmp_dir do
+      conf = "--gte 90,99"
+      conf_file = File.open('.cane', 'w') {|f| f.write conf }
 
-    Cane::File.stub(exists?: true, contents: File.read(conf_file))
+      task = Cane::RakeTask.new(:canefile_quality)
 
-    task = Cane::RakeTask.new(:canefile_quality)
+      task.should_receive(:abort)
+      out = capture_stdout do
+        Rake::Task['canefile_quality'].invoke
+      end
 
-    task.should_receive(:abort)
-    out = capture_stdout do
-      Rake::Task['canefile_quality'].invoke
+      out.should include("Quality threshold crossed")
     end
-
-    out.should include("Quality threshold crossed")
   end
 
   after do
