5bcf00013eee398a469a69265d5751df98443085
Chuck LeDuc Díaz <chuck.leduc@sage.com>
2013-04-17 17:05:17 +0200

    Add textile as an allowed README extension

--- lib/cane/doc_check.rb
+++ lib/cane/doc_check.rb
@@ -62,7 +62,7 @@ module Cane
       return result if opts[:no_readme]
 
       filenames = ['README', 'readme']
-      extensions = ['', '.txt', '.md', '.mdown', '.rdoc', '.markdown']
+      extensions = ['', '.txt', '.md', '.mdown', '.rdoc', '.markdown', '.textile']
       combinations = filenames.product(extensions)
 
       if combinations.none? {|n, x| Cane::File.exists?(n + x) }
