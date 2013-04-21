3d74bce135d886da5d96d20f569591df89fb1166
Xavier Shay <xavier@rhnh.net>
2013-04-21 09:33:39 -0700

    Make glob test deterministic to fix build.

--- spec/file_spec.rb
+++ spec/file_spec.rb
@@ -10,14 +10,14 @@ describe Cane::File do
         README
         readme.md
         ReaDME.TEXTILE
-      )
+      ).sort
 
       Dir.mktmpdir do |dir|
         Dir.chdir(dir) do
           expected.each do |x|
             FileUtils.touch(x)
           end
-          Cane::File.case_insensitive_glob("README*").should == expected
+          Cane::File.case_insensitive_glob("README*").sort.should == expected
         end
       end
     end
