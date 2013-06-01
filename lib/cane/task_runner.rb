6f1777338fc05edfaf62cc066921747e605e2588
Chris Hunt <c@chrishunt.co>
2013-06-01 10:46:15 -0700

    Fix spelling of parallelism in comment

--- lib/cane/task_runner.rb
+++ lib/cane/task_runner.rb
@@ -9,7 +9,7 @@ module Cane
   end
   module_function :task_runner
 
-  # Mirrors the Parallel gem's interface but does not provide any parralleism.
+  # Mirrors the Parallel gem's interface but does not provide any parallelism.
   # This is faster for smaller tasks since it doesn't incur any overhead for
   # creating new processes and communicating between them.
   class SimpleTaskRunner
