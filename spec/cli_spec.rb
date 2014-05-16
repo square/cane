bf42478ab6fbd7f26fbf9819113f2259e4744af5
Logan Hasson <logan.hasson@gmail.com>
2014-05-15 22:50:11 -0400

    Update specs to RSpec 2.14.7 syntax
    This conversion is done by Transpec 1.13.1 with the following command:
        transpec
    
    * 112 conversions
        from: obj.should
          to: expect(obj).to
    
    * 66 conversions
        from: == expected
          to: eq(expected)
    
    * 12 conversions
        from: obj.should_receive(:message)
          to: expect(obj).to receive(:message)
    
    * 6 conversions
        from: obj.should_not
          to: expect(obj).not_to
    
    * 1 conversion
        from: -> { }.should
          to: expect { }.to
    
    * 1 conversion
        from: =~ [1, 2]
          to: match_array([1, 2])

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -9,16 +9,16 @@ describe Cane::CLI do
     let!(:cane)   { class_double("Cane").as_stubbed_const }
 
     it 'runs Cane with the given arguments' do
-      parser.should_receive(:parse).with("--args").and_return(args: true)
-      cane.should_receive(:run).with(args: true).and_return("tracer")
+      expect(parser).to receive(:parse).with("--args").and_return(args: true)
+      expect(cane).to receive(:run).with(args: true).and_return("tracer")
 
-      described_class.run("--args").should == "tracer"
+      expect(described_class.run("--args")).to eq("tracer")
     end
 
     it 'does not run Cane if parser was able to handle input' do
-      parser.should_receive(:parse).with("--args").and_return("tracer")
+      expect(parser).to receive(:parse).with("--args").and_return("tracer")
 
-      described_class.run("--args").should == "tracer"
+      expect(described_class.run("--args")).to eq("tracer")
     end
   end
 end
